bc5752063a1062b07b6aa5e63b0ca3c4f53444cb
renweiwuming <renwei_wuming@sina.com>
2018-06-25 15:54:50 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+print(tf.__version__)
+hello = tf.constant('hello world',dtype='string')
+session = tf.Session()
+print(session.run(hello))
+a = tf.constant(11)
+b = tf.constant(39)
+print(session.run(a+ b))
+
